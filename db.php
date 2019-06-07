3ac32ec98fe04fe2ebf2709cb9d37c9dd5d2db5f
Amtbusk7764 <48216285+Amtbusk7764@users.noreply.github.com>
2019-06-07 01:59:28 -0700

    Update db.php

--- db.php
+++ db.php
@@ -5,8 +5,8 @@ class DB{
 		
 		
 		
-		      $this->connection = mysql_connect('remotemysql.com', 'y6dU5Zv0wc', 'tKnXT7Eat6') or die(mysql_error());
-				mysql_select_db('y6dU5Zv0wc', $this->connection) or die(mysql_error());
+		      $this->connection = mysql_connect(':/cloudsql/thinking-leaf-243001:us-central1:shipryt', 'antbusk', 'Window167764$') or die(mysql_error());
+				mysql_select_db('shipryt', $this->connection) or die(mysql_error());
 		
 		
 		
